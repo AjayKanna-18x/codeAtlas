3414a3a15027d6f160f0915e0de502b73dec32c1
Ajay Kanna <ajaykanna265@gmail.com>
2026-06-24 13:19:24 +0530

    graprouts

--- backend/src/routes/graphRoutes.js
+++ backend/src/routes/graphRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getDependencyGraph } from "../controllers/graphController.js";
+import {
+  getDependencyGraph,
+  getGraphStats,
+  getNodeDetails,
+} from "../controllers/graphController.js";
 
 const router = express.Router();
 
 router.get("/:repoId", getDependencyGraph);
+router.get("/:repoId/stats", getGraphStats);
+router.get("/:repoId/node/:nodeId", getNodeDetails);
 
 export default router;
