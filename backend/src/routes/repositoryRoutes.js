a34dcf9bb6679b22f8200e2bef1a1f44588bc595
Ajay Kanna <ajaykanna265@gmail.com>
2026-06-24 11:57:39 +0530

    updated repo in routes  backend

--- backend/src/routes/repositoryRoutes.js
+++ backend/src/routes/repositoryRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getAllRepositories,
   importRepository,
+  validateRepo,
 } from "../controllers/repositoryController.js";
 
 const router = express.Router();
 
 router.get("/", getAllRepositories);
+router.post("/validate", validateRepo);
 router.post("/import", importRepository);
 
 export default router;
