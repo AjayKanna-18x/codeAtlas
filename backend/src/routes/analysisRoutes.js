b908757e9cd68b9b2726ee40e3ef78bb4152dbad
Ajay Kanna <ajaykanna265@gmail.com>
2026-06-24 13:19:44 +0530

    anzrouts

--- backend/src/routes/analysisRoutes.js
+++ backend/src/routes/analysisRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getAnalysisResult } from "../controllers/analysisController.js";
+import {
+  getAnalysisResult,
+  runDeadCodeDetection,
+  getAnalysisSummary,
+} from "../controllers/analysisController.js";
 
 const router = express.Router();
 
 router.get("/:repoId", getAnalysisResult);
+router.get("/:repoId/summary", getAnalysisSummary);
+router.post("/:repoId/deadcode", runDeadCodeDetection);
 
 export default router;
