adcdeb260d6f430581a2994ccd209a330232e5dd
Ajay Kanna <ajaykanna265@gmail.com>
2026-06-24 13:19:09 +0530

    reprouts

--- backend/src/routes/repositoryRoutes.js
+++ backend/src/routes/repositoryRoutes.js
@@ -4,13 +4,17 @@ import {
   getRepositoryById,
   importRepository,
   validateRepo,
+  analyzeRepository,
+  deleteRepository,
 } from "../controllers/repositoryController.js";
 
 const router = express.Router();
 
 router.get("/", getAllRepositories);
 router.get("/:id", getRepositoryById);
+router.delete("/:id", deleteRepository);
 router.post("/validate", validateRepo);
 router.post("/import", importRepository);
+router.post("/:id/analyze", analyzeRepository);
 
 export default router;
